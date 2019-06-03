fd7ac8a65f659546586aed2eda2cc2ba01b57dc5
Leo Zimmerman <leozimmerman@gmail.com>
2019-06-03 16:00:57 -0300

    Created repository, Xcode project and gitignore file

---
+++ MovieGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieGallery
+//
+//  Created by Leonardo Zimmerman on 03/06/2019.
+//  Copyright © 2019 Leonardo Zimmerman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
